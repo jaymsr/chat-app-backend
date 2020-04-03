0957913810354cd93675bf875ce59bd80dd3c574
timeac131 <tyongyai@hotmail.com>
2020-04-03 22:09:36 +0700

    connect to mongo server

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ http.createServer(app).listen(3000, function () {
 app.get("*", function (req, res) {
     res.send("Hello World");
 })
+async function main(){
+    try{
+
+        const MongoClient = require('mongodb').MongoClient;
+        const uri = "mongodb+srv://admin:parallel2020@cluster0-cegct.mongodb.net/test?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+        await client.connect();
+        const find = await client.db("Parallel").collection("Users").findOne({ //test query
+            _id : 5000
+        });
+        console.log(find)
+
+    }catch(e){
+        console.error(e)
+    }
+
+}
+main().catch(console.error);
