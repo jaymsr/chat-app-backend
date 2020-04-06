582de288a7bdecaf4f89ca57a6bd941d06e18178
Nai131 <Natthapath47005@gmail.com>
2020-04-06 22:47:10 +0700

    up

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on("connection", function (socket) {
         console.log(msg);
         var {
             username,
-            groupname
+            groupName
         } = msg;
         var group = {
             groupName: groupName
